df13b1c87277a6d2195f5be360a9573d7db7bbe3
Aryan <durgearyan9@gmail.com>
2024-11-22 18:40:16 +0530

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "ARYAN DURGE", // Your name
+  profile: "/profile.png", // Path to your profile picture
+  designation: "Software Developer Intern", // Your job title or designation
+  description: "I am Aryan Durge, a passionate software developer with expertise in creating intuitive and dynamic applications.", // A short bio or description about you
+  email: "aryandurge@example.com", // Your email address
+  phone: "+91 93923 04794", // Your phone number
+  address: "Hyderabad, India", // Your address (optional)
+  github: "https://github.com/ArynaDurge", // Link to your GitHub profile
+  facebook: "https://facebook.com/yourfacebookprofile", // Link to your Facebook profile
+  linkedIn: "https://linkedin.com/in/aryandurge", // Link to your LinkedIn profile
+  twitter: "https://X.com/AryanDurge", // Link to your Twitter profile
+  stackOverflow: "https://stackoverflow.com/users/yourstackid", // Link to your Stack Overflow profile
+  leetcode: "https://leetcode.com/aryandurge", // Link to your LeetCode profile
+  devUsername: "Aryan Durge", // Your dev.to username for fetching blogs (if applicable)
+  resume: "", // Path or URL to your resume
+};
